fbc885b1b2f8ad9800ba36765725719d936b9f98
Jean-Baptiste Taffin <taffin.jb@gmail.com>
2022-05-31 20:34:59 +0200

    seeds2

--- db/seeds.rb
+++ db/seeds.rb
@@ -61,9 +61,298 @@ end
 
 #Create plant instances
 # famille, espèces, description, location
-until n == 20 do
+for n in 1..19 do
   Plant.create(family:array_title[n], species:array_family[n], site_name:"le wagon", description:array_description[n], user_id: 1)
 end
 
+csv_1_1 = File.read(Rails.root.join('db', 'data_1_1.csv'))
 
-# Create
+BULK_SIZE = 500
+
+device_1 = Device.create!(
+  name: "Environment #1",
+  category: "capteur",
+  status: true,
+  temperature: true,
+  air_rh: true,
+  ground_rh: false,
+  luminosity: false,
+  volume: false,
+  tank_level: false
+)
+
+device_2 = Device.create!(
+  name: "Soil Humidity #1",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: true,
+  luminosity: false,
+  volume: false,
+  tank_level: false
+)
+
+device_3 = Device.create!(
+  name: "Tank Level #1",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: false,
+  volume: false,
+  tank_level: true
+)
+
+device_4 = Device.create!(
+  name: "Débitmètre #1",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: false,
+  volume: true,
+  tank_level: false
+)
+
+device_5 = Device.create!(
+  name: "Luminance #1",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: true,
+  volume: false,
+  tank_level: false
+)
+
+device_6 = Device.create!(
+  name: "Environment #2",
+  category: "capteur",
+  status: true,
+  temperature: true,
+  air_rh: true,
+  ground_rh: false,
+  luminosity: false,
+  volume: false,
+  tank_level: false
+)
+
+device_7 = Device.create!(
+  name: "Soil Humidity #2",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: true,
+  luminosity: false,
+  volume: false,
+  tank_level: false
+)
+
+device_8 = Device.create!(
+  name: "Tank Level #2",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: false,
+  volume: false,
+  tank_level: true
+)
+
+device_9 = Device.create!(
+  name: "Débitmètre #2",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: false,
+  volume: true,
+  tank_level: false
+)
+
+device_10 = Device.create!(
+  name: "Luminance #2",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: true,
+  volume: false,
+  tank_level: false
+)
+
+device_11 = Device.create!(
+  name: "Environment #3",
+  category: "capteur",
+  status: true,
+  temperature: true,
+  air_rh: true,
+  ground_rh: false,
+  luminosity: false,
+  volume: false,
+  tank_level: false
+)
+
+device_12 = Device.create!(
+  name: "Soil Humidity #3",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: true,
+  luminosity: false,
+  volume: false,
+  tank_level: false
+)
+
+device_13 = Device.create!(
+  name: "Tank Level #3",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: false,
+  volume: false,
+  tank_level: true
+)
+
+device_14 = Device.create!(
+  name: "Débitmètre #3",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: false,
+  volume: true,
+  tank_level: false
+)
+
+device_15 = Device.create!(
+  name: "Luminance #3",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: true,
+  volume: false,
+  tank_level: false
+)
+
+device_16 = Device.create!(
+  name: "Environment #4",
+  category: "capteur",
+  status: true,
+  temperature: true,
+  air_rh: true,
+  ground_rh: false,
+  luminosity: false,
+  volume: false,
+  tank_level: false
+)
+
+device_17 = Device.create!(
+  name: "Soil Humidity #4",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: true,
+  luminosity: false,
+  volume: false,
+  tank_level: false
+)
+
+device_18 = Device.create!(
+  name: "Tank Level #4",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: false,
+  volume: false,
+  tank_level: true
+)
+
+device_19 = Device.create!(
+  name: "Débitmètre #4",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: false,
+  volume: true,
+  tank_level: false
+)
+
+device_20 = Device.create!(
+  name: "Luminance #4",
+  category: "capteur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: true,
+  volume: false,
+  tank_level: false
+)
+
+device_21 = Device.create!(
+  name: "pompe d'arrosage #1",
+  category: "actionneur",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: false,
+  volume: false,
+  tank_level: false
+)
+
+device_22 = Device.create!(
+  name: "webcam #1",
+  category: "camera",
+  status: true,
+  temperature: false,
+  air_rh: false,
+  ground_rh: false,
+  luminosity: false,
+  volume: false,
+  tank_level: false
+)
+
+# metrics = []
+# CSV.foreach(csv_1_1, :headers => true, :encoding => 'ISO-8859-1') do
+#   metric = DeviceMetric.new(
+#     device: device_1,
+#     temperature: row['temperature'],
+#     air_rh: row['air_rh'],
+#     ground_rh: row['ground_rh'],
+#     luminosity: row['luminosity'],
+#     tank_level: row['tank_level'],
+#     volume: row['volume']
+#   )
+
+#   metrics << metric
+
+#   if metrics.size >= BULK_SIZE
+#     DeviceMetric.import metrics
+#     metrics = []
+#   end
+# end
+
+# if metrics.size > 0
+#   DeviceMetric.import metrics
+# end
