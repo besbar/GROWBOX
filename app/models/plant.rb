afbd41b9f9f3c34b9033eea29d14b19924205e77
Jean Guillet <jean.guillet1@gmail.com>
2022-05-31 17:09:58 +0200

    create models AlertSetting, Device, Plant and User

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,4 +2,8 @@ class Plant < ApplicationRecord
   belongs_to :user
   has_many :plant_devices
   has_many :devices, through: :plant_devices
+
+  validates :family, presence: true
+  validates :species, presence: true
+  validates :site_name, presence: true
 end
