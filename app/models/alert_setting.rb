303b6e6a711bc8c3f2a2cebaa27350a9a12f1287
Edouard de Feraudy <edouarddeferaudy@gmail.com>
2022-06-02 17:41:52 +0200

    alertes createshowdelete

--- app/models/alert_setting.rb
+++ app/models/alert_setting.rb
@@ -3,6 +3,9 @@ class AlertSetting < ApplicationRecord
 
   validates :tank_level_min, inclusion: { in: [0, 1] }, allow_nil: true
 
+  validates :temperature_min, numericality: { greater_than_or_equal: 0 }, allow_nil: true
+  validates :temperature_max, numericality: { greater_than: :temperature_min }, allow_nil: true
+
   validates :ground_rh_min, numericality: { greater_than_or_equal: 0 }, allow_nil: true
   validates :ground_rh_max, numericality: { greater_than: :ground_rh_min }, allow_nil: true
 
@@ -17,7 +20,7 @@ class AlertSetting < ApplicationRecord
   private
 
   def at_least_one_threshold
-    fields = [tank_level_min, ground_rh_min, ground_rh_max, luminosity_min, luminosity_max, air_rh_min, air_rh_max]
+    fields = [temperature_min, temperature_max, tank_level_min, ground_rh_min, ground_rh_max, luminosity_min, luminosity_max, air_rh_min, air_rh_max]
     errors.add(:base, "Doit contenir au moins un palier") if fields.all?(&:nil?)
   end
 end
