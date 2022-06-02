f1465413f19ae01cf7483160255b76af3d8afa13
Paul Portier <paulportier@gmail.com>
2022-06-02 16:01:42 +0200

    Break client get data

--- app/services/mqtt_data_builder.rb
+++ app/services/mqtt_data_builder.rb
@@ -9,21 +9,22 @@ class MqttDataBuilder
 
   def initialize
     @client = {
-      :host => '10.20.1.137',
+      host: '10.20.1.137',
       #:host => 'test.mosquitto.org',
       #:host => '192.168.1.105',
-      :port => 1883,
-      :username => "jb",
-      :password => "growbox"
+      port: 1883,
+      username: "jb",
+      password: "growbox"
     }
   end
 
   def build_data
-    Thread.new do
     MQTT::Client.connect(@client) do |c|
 
-      c.subscribe('air_temp_4', 'air_humi_4', 'soil_humi_4', 'flow_meter_4', 'tank_level_4')
+      c.subscribe('air_temp_4', 'air_humi_4', 'soil_humi_4', 'flow_meter_4', 'tank_level_4', 'end_call')
+
       c.publish('sensors_4', 'test')
+
       metric_name_per_channel_name = {
         'air_temp' => 'temperature',
         'air_humi' => 'air_rh',
@@ -31,6 +32,7 @@ class MqttDataBuilder
         'flow_meter' => 'volume',
         'tank_level' => 'tank_level'
       }
+
       topic_per_device_id = {
         'air_temp_4' => Device.first,
         'air_humi_4' => Device.first,
@@ -38,23 +40,22 @@ class MqttDataBuilder
         'flow_meter_4' => Device.find(4),
         'tank_level_4' => Device.find(3)
       }
-      c.get do |topic,message|
-        puts "#{topic}: #{message}"
+
+      data = {}
+
+      c.get do |topic, message|
+        data[topic] = message
         if ['air_temp_4', 'air_humi_4', 'soil_humi_4', 'flow_meter_4', 'tank_level_4'].include?(topic)
           match_data = topic.match(/(?<channel_name>[a-z_]+)_(?<channel_id>\d)/)
           channel_name = match_data[:channel_name]
-          puts channel_name
           channel_id = match_data[:channel_id]
           device = topic_per_device_id[topic]
-          puts device.name
           metric_name = metric_name_per_channel_name[channel_name]
-          puts metric_name
           puts DeviceMetric.create(device: device, metric_name => message)
         end
+        break if topic == 'end_call' && message == '1'
       end
+      return data
     end
-    Thread.exit
   end
-end
-
 end
