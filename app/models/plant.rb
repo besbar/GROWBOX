34309bf49920e9d77a0eb82e57d7cb95d135f029
Edouard de Feraudy <edouarddeferaudy@gmail.com>
2022-06-03 15:20:13 +0200

    simple_formchanged language & family content

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,9 +1,12 @@
 class Plant < ApplicationRecord
+  FAMILY = ["Aglaonema", "Monstera", "Philodendron Scandens", "Pothos", "Zamioculcas Zamiifolia"]
+  SITE_NAME = ["Le Wagon", "Jardins de Versailles", "Jardin des Plantes", "Jardin du Luxembourg"]
+
   belongs_to :user
   has_many :plant_devices
   has_many :devices, through: :plant_devices
 
-  validates :family, presence: true, inclusion: { in: ["Aglaonema", "Monstera", "Philodendron Scandens", "Pothos", "Zamioculcas Zamiifolia"] }
+  validates :family, presence: true, inclusion: { in: FAMILY }
   validates :species, presence: true
-  validates :site_name, presence: true
+  validates :site_name, presence: true, inclusion: { in: SITE_NAME }
 end
