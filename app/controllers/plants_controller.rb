22dba274435b14d6333947c07bbaddc58c40718b
Jean Guillet <jean.guillet1@gmail.com>
2022-06-07 17:40:11 +0200

    adding refresh_latest Stimulus controller to live update temperature, air_rh and ground_rh latest data

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,6 +1,7 @@
 class PlantsController < ApplicationController
-  before_action :set_plant, only: %i[show edit update for_temperature_air_rh for_ground_rh]
-  before_action :set_attached_devices, only: %i[show for_temperature_air_rh for_ground_rh]
+  before_action :set_plant, only: %i[show edit update for_temperature_air_rh for_ground_rh metrics]
+  before_action :set_attached_devices, only: %i[show for_temperature_air_rh for_ground_rh metrics]
+  before_action :set_metrics, only: %i[show metrics]
 
   def index
     @plants = Plant.all
@@ -15,18 +16,6 @@ class PlantsController < ApplicationController
 
   def show
     return if @attached_devices.empty?
-
-    # Get temperature and relative air humidity
-    @temperature_air_rh = set_temperature_air_rh
-    @temperature_latest = latest_non_nil_from(array: @temperature_air_rh, dataset: 0) if @temperature_air_rh
-    @air_rh_latest = latest_non_nil_from(array: @temperature_air_rh, dataset: 1) if @temperature_air_rh
-
-    # Get relative ground humidity
-    @ground_rh = set_ground_rh
-    @ground_rh_latest = @ground_rh.last[1] if @ground_rh
-
-    # Get latest tank level
-    @tank_level_latest = set_latest_tank_level
   end
 
   def new
@@ -64,6 +53,17 @@ class PlantsController < ApplicationController
     render json: set_ground_rh
   end
 
+  def metrics
+    render json: {
+      temperature_air_rh: render_to_string(
+        partial: "temperature_air_rh",
+        locals: { temperature_latest: @temperature_latest, air_rh_latest: @air_rh_latest }
+      ),
+      tank_level: render_to_string(partial: "tank_level", locals: { tank_level_latest: @tank_level_latest }),
+      ground_rh: render_to_string(partial: "ground_rh", locals: { ground_rh_latest: @ground_rh_latest })
+    }
+  end
+
   private
 
   def set_plant
@@ -81,6 +81,20 @@ class PlantsController < ApplicationController
     @attached_devices = @plant.devices.empty? ? [] : @plant.devices
   end
 
+  def set_metrics
+    # Get temperature and relative air humidity
+    @temperature_air_rh = set_temperature_air_rh
+    @temperature_latest = latest_non_nil_from(array: @temperature_air_rh, dataset: 0) if @temperature_air_rh
+    @air_rh_latest = latest_non_nil_from(array: @temperature_air_rh, dataset: 1) if @temperature_air_rh
+
+    # Get relative ground humidity
+    @ground_rh = set_ground_rh
+    @ground_rh_latest = @ground_rh.last[1] if @ground_rh
+
+    # Get latest tank level
+    @tank_level_latest = set_latest_tank_level
+  end
+
   def plant_params
     params.require(:plant).permit(:family, :species, :site_name, :description, :device_ids)
   end
@@ -90,10 +104,10 @@ class PlantsController < ApplicationController
 
     temperature = { name: "Température (°C)", data: [] }
     air_rh = { name: "Humidité air (%)", data: [] }
-    @attached_devices.where(temperature: true).first.device_metrics.last(120).map do |data|
+    @attached_devices.where(temperature: true).first.device_metrics.last(60).map do |data|
       temperature[:data] << [data.created_at, data.temperature]
     end
-    @attached_devices.where(air_rh: true).first.device_metrics.last(120).map do |data|
+    @attached_devices.where(air_rh: true).first.device_metrics.last(60).map do |data|
       air_rh[:data] << [data.created_at, data.air_rh]
     end
     [temperature, air_rh]
@@ -102,7 +116,7 @@ class PlantsController < ApplicationController
   def set_ground_rh
     return unless @attached_devices.where(ground_rh: true).count.nonzero?
 
-    @attached_devices.where(ground_rh: true).first.device_metrics.last(120).map do |data|
+    @attached_devices.where(ground_rh: true).first.device_metrics.last(60).map do |data|
       [data.created_at, data.ground_rh]
     end
   end
