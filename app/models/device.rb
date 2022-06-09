30a99cd71bf799e17ff8cc4d215aee4b8f53324f
Jean Guillet <46451676+John35961@users.noreply.github.com>
2022-06-09 16:00:15 +0200

    Update device.rb
    Removing SUBCATEGORYB

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,6 @@
 class Device < ApplicationRecord
   CATEGORY = ["capteur", "actionneur", "caméra"]
   SUBCATEGORY = ["capteur", "environnement", "humidité sol", "débitmètre", "luminosité", "réservoir", "arrosage", "caméra"]
-  SUBCATEGORYB = { "capteur" => ["environnement", "humidité sol", "débitmètre", "luminosité", "réservoir"], "actionneur" => "arrosage", "caméra" => "caméra" }
 
   belongs_to :user
   has_many :device_metrics
