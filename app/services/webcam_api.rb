1e253170d36359342337cc930ef32f53978098c4
Jean-Baptiste Taffin <taffin.jb@gmail.com>
2022-06-09 10:27:31 +0200

    mqtt_setup

--- app/services/webcam_api.rb
+++ app/services/webcam_api.rb
@@ -1,6 +1,6 @@
-require "open-uri"
-require "base64"
+# require "open-uri"
+# require "base64"
 
-url = '10.20.0.1:2000/cgi-bin/api.cgi?cmd=Snap&channel=0&rs=wuuPhkmUCeI9WG7C&user=admin&password=growbox'
-image = URI.open(url).read
-image_64 = Base64.encode64(image)
+# url = '10.20.0.1:2000/cgi-bin/api.cgi?cmd=Snap&channel=0&rs=wuuPhkmUCeI9WG7C&user=admin&password=growbox'
+# image = URI.open(url).read
+# image_64 = Base64.encode64(image)
